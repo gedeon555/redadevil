3ff01b3f89e1d5c773349c3da287b771945cfcdc
julien.assemat <julien.assemat@9962e527-cf3e-0410-bf94-21978dc8b85e>
2007-11-27 10:51:14 +0000

    * Updating 'mfcc.sh' Shell script (handle of arguments error). * Adding configuration file for HCopy in 'conf' directory.

--- trunk/scripts/mfcc.sh
+++ trunk/scripts/mfcc.sh
@@ -6,8 +6,13 @@
 ## Login   <homerlan@homerlan>
 ## 
 ## Started on  Mon Nov 26 16:06:55 2007 Homerlan
-## Last update Mon Nov 26 16:15:35 2007 Homerlan
+## Last update Tue Nov 27 11:41:17 2007 Homerlan
 ##
+
+if [ $# -lt 2 ]; then
+    echo "usage: mfcc.sh sph_files_dir htk_config_file";
+    exit 0
+fi
 
 for i in $1/*.sph;
 do
