f009cd090c692c255f8a327a23911c753ba8189a
julien.assemat <julien.assemat@9962e527-cf3e-0410-bf94-21978dc8b85e>
2008-01-11 13:31:22 +0000

    * End exo1.

--- src/learn1.m
+++ src/learn1.m
@@ -4,6 +4,9 @@ dir = '/home/homerlan/Desktop/Reda/redadevil';
 filename = [dir '/data/dev-pitch.txt'];
 data = load(filename);
 
+% 1 = female
+% 0 = male
+
 data_female = data(find(data(:, 2) == 1))
 data_male = data(find(data(:, 2) == 0))
 
@@ -20,7 +23,32 @@ g = gmmem(g, data_female, o);
 h = gmminit(h, data_male, o);
 h = gmmem(h, data_male, o);
 
-x = 80:1:400;
-plot(x, gaussmf(x, [ sqrt(g.covars) g.centres ]));
-hold on;
-plot(gaussmf(x, [ sqrt(h.covars) h.centres ]));
+sigma_female = sqrt(g.covars);
+mu_female = g.centres;
+sigma_male = sqrt(h.covars);
+mu_male = h.centres;
+
+threshold = ((mu_female - sigma_female * 2) + (mu_male + sigma_male * 2)) / 2;
+
+errors = 0;
+total = 0;
+for i=1:size(data, 1)
+    if data(i, 1) <= threshold
+        fprintf('Male 0 == %i (%i)\n', data(i, 2), round(data(i, 1) - threshold));
+        if data(i, 2) == 1
+           errors = errors + 1; 
+        end
+    else
+        fprintf('Female 1 == %i (%i)\n', data(i, 2), round(data(i, 1) - threshold));  
+        if data(i, 2) == 0
+           errors = errors + 1; 
+        end
+    end
+    total = total + 1;
+end
+fprintf('errors: %i/%i (%i percents)\n', errors, total, round(100 * errors / total));
+
+%x = 0:1:400;
+%plot(x, gaussmf(x, [ sqrt(g.covars) g.centres ]));
+%hold on;
+%plot(gaussmf(x, [ sqrt(h.covars) h.centres ]));
