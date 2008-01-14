f32fc0cafa8390d60aca246ed59a9571d08a286b
davidlandais <davidlandais@9962e527-cf3e-0410-bf94-21978dc8b85e>
2008-01-14 10:54:33 +0000

    * Exo 2, Apprend_locuteur 2 et 3, premiers tests

--- src/apprend_locuteur.m
+++ src/apprend_locuteur.m
@@ -1,28 +1,10 @@
 function g = apprend_locuteur (nom_de_fichier)
-    %mfccs = load(nom_de_fichier);
-    mfccs1 = load('../data/ntst_11/speech-sp01.mfcc');
-    mfccs2 = load('../data/ntst_11/speech-sp02.mfcc');
-    mfccs3 = load('../data/ntst_11/speech-sp03.mfcc');
-    mfccs4 = load('../data/ntst_11/speech-sp04.mfcc');
-    mfccs5 = load('../data/ntst_11/speech-sp05.mfcc');
-    mfccs6 = load('../data/ntst_11/speech-sp06.mfcc');
-    mfccs7 = load('../data/ntst_11/speech-sp07.mfcc');
-    mfccs8 = load('../data/ntst_11/speech-sp08.mfcc');
-    mfccs9 = load('../data/ntst_11/speech-sp08.mfcc');
-    mfccs10 = load('../data/ntst_11/speech-sp10.mfcc');
-    mfccs11 = load('../data/ntst_11/speech-sp11.mfcc');
-    mfccs = [mfccs1;mfccs2;mfccs3;mfccs4;mfccs5;mfccs6;mfccs7;mfccs8;mfccs9;mfccs10;mfccs11];
-    
-    %[r, c, v] = find(mfccs == 0);
-    %r
-    %non_nuls = find(mfccs(:, 1) == 0 & mfccs(:, 2) == 0 &mfccs(:, 3) == 0 & mfccs(:, 4) == 0 & mfccs(:, 5) == 0 & mfccs(:, 6) == 0 & mfccs(:, 7) == 0 & mfccs(:, 8) == 0 & mfccs(:, 9) == 0 & mfccs(:, 10) == 0 & mfccs(:, 11) == 0 & mfccs(:, 12) == 0 & mfccs(:, 13) == 0 & mfccs(:, 14) == 0 & mfccs(:, 15) == 0);
-    %non_nuls
-    %mfccs_non_nuls = mfccs(non_nuls, 1:15);
-    g = gmm(15, 255, 'diag');
-    
+    mfccs = load(nom_de_fichier);
+    g = gmm(16, 64, 'diag');
     o = zeros(1, 16);
     o(1) = 1;
     o(14) = 20;
-    g = gmminit(g, mfccs(:, 1:15), o);
-    g = gmmem(g, mfccs(:, 1:15), o);
+    o(5) = 1;
+    g = gmminit(g, mfccs, o);
+    g = gmmem(g, mfccs, o);
 end
--- src/exo_2.m
+++ src/exo_2.m
@@ -1,2 +1,35 @@
 addpath '../lib/netlab';
-apprend_locuteur_2('../speech-sp01.mfcc');
+%addpath '../lib/gmmbayestb-v1.0'
+%addpath '../lib/stats/gmdistribution'
+
+fichiers_apprentissage = ['sp02'; 'sp05'; 'sp08'; 'sp11'; 'sp03'; 'sp06'; 'sp09'; 'sp01'; 'sp04'; 'sp07'; 'sp10'];
+
+for numero = 1:size(fichiers_apprentissage)
+    nom_du_fichier = fichiers_apprentissage(numero, :);
+    g = apprend_locuteur(['../data/ntst_11/speech-' nom_du_fichier '.mfcc']);
+    save (['../data/ntst_11/speech-' nom_du_fichier '.gmm'], 'g');
+end
+
+
+for numero = 1:size(fichiers_apprentissage)
+    nom_du_fichier = fichiers_apprentissage(numero, :);
+    load ('-mat', ['../data/ntst_11/speech-' nom_du_fichier '.gmm'], 'g');
+    tab_g(numero) = g;
+end
+
+fichiers_tests = ['cw'; 'ed'; 'eh'; 'eo'; 'er'; 'fd'; 'fh'; 'fl'; 'fp'; 'fu'; 'fx'; 'gc'; 'gf'; 'gi'; 'gl'; 'go'; 
+        'du'; 'ef'; 'ek'; 'ep'; 'ex'; 'fe'; 'fi'; 'fm'; 'fq'; 'fv'; 'ga'; 'gd'; 'gg'; 'gj'; 'gm'; 'gp'; 
+        'dw'; 'eg'; 'el'; 'eq'; 'ez'; 'fg'; 'fj'; 'fn'; 'fs'; 'fw'; 'gb'; 'ge'; 'gh'; 'gk'; 'gn'];
+
+
+for numero = 1:10%size(fichiers_tests)
+    nom_du_fichier = fichiers_apprentissage(numero, :);
+    fprintf('%s...\n', nom_du_fichier);
+    mfccs = load(['../data/ntst_11/speech-' nom_du_fichier '.mfcc']);
+    for i=1:size(tab_g, 2)
+        fprintf('%i .. ', i);
+        result(numero, i) = mean(gmmprob(tab_g(i), mfccs));
+    end
+    fprintf('\n');
+    fprintf('%s => %s\n', nom_du_fichier, fichiers_apprentissage(find(result(numero,:) == max(result(numero, :))), :));
+end
